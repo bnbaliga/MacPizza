84850683ece86e944e2e847f34b0d70301f274a5
Narasimha Baliga <bnbaliga@gmail.com>
2021-05-14 18:30:00 -0500

    Initial Commit

---
+++ MacPizza/MacPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPizzaApp.swift
+//  MacPizza
+//
+//  Created by Narasimha Baliga on 5/14/21.
+//
+
+import SwiftUI
+
+@main
+struct MacPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
